bd9f00ea8d6a6634fb98fdcff8955bfc1adbd9f5
miiguelean <mitwi26@gmail.com>
2023-01-29 21:13:10 -0600

    la interfaz casi terminada

--- prod_usados.php
+++ prod_usados.php
@@ -13,6 +13,7 @@ if (isset($_POST['btnOK']) == 'OK') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
+    <!-- <link rel="stylesheet" href="css/scroll.css"> -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
@@ -20,9 +21,19 @@ if (isset($_POST['btnOK']) == 'OK') {
 </head>
 <body class="fondo">
    <p class="prod_uso">Productos en uso</p>
-    
+   <div class="scroll_list">
+        <button class="box"><img src="img/volleyball.png" alt="VolleyBall"><h2>Miguel Tienda<br>TIADSM 5B</h2><div id="clock_wrapper"><canvas id="clock" width="30" height="30"></canvas><div id="timer"></div></div></button>
+        <button class="box"><img src="img/balon-fut.png" alt="Futbol"><h2>Miguel Tienda<br>TIADSM 5B</h2></button>
+        <button class="box"><img src="img/baloncesto.png" alt="Baloncesto"><h2>Miguel Tienda<br>TIADSM 5B</h2></button>
+        <button class="box"><img src="img/ajedrez.png" alt="Ajedrez"><h2>Miguel Tienda<br>TIADSM 5B</h2></button>
+        <button class="box"><img src="img/guitar.png" alt="Guitarra"><h2>Miguel Tienda<br>TIADSM 5B</h2></button>
+        <button class="box"><img src="img/saxofon.png" alt="Saxofon"><h2>Miguel Tienda<br>TIADSM 5B</h2></button>
+        <button class="box"><img src="img/acordeon.png" alt="Acordeon"><h2>Miguel Tienda<br>TIADSM 5B</h2></button>
+        <button class="box"><img src="img/casaca.png" alt="Casaca"><h2>Miguel Tienda<br>TIADSM 5B</h2></button>
+    </div>
     <div>
     <a class="btnvolver" href="selecciona_cat.php">VOLVER</a>
     </div>
+    <script src="js/scroll_list.js"></script>
 </body>
 </html>
