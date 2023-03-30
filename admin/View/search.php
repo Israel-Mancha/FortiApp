63f01a9f535ae5182ce1d3957ee755906db8e75c
miigulean <mitwi26@gmail.com>
2023-03-29 21:49:40 -0500

    si

--- admin/View/search.php
+++ admin/View/search.php
@@ -5,41 +5,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles-form.css">
     <link rel="stylesheet" href="../css/styles.css">
     <title>Productos</title>
     <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
 
-        th,
-        td {
-            text-align: left;
-            padding: 8px;
-            border-bottom: 1px solid #ddd;
-        }
+    th,
+    td {
+        text-align: left;
+        padding: 8px;
+        border-bottom: 1px solid #ddd;
+    }
 
-        tr:hover {
-            background-color: #f5f5f5;
-        }
+    tr:hover {
+        background-color: #f5f5f5;
+    }
 
-        th {
-            background-color: #4CAF50;
-            color: white;
-        }
+    th {
+        background-color: #4CAF50;
+        color: white;
+    }
     </style>
     <script>
-        function eliminar(id_producto) {
-            if (confirm("¿Está seguro de que desea eliminar este producto?")) {
-                window.location.href = "delete.php?id=" + id_producto;
-            }
+    function eliminar(id_producto) {
+        if (confirm("¿Está seguro de que desea eliminar este producto?")) {
+            window.location.href = "delete.php?id=" + id_producto;
         }
+    }
 
-        function modificar(id_producto) {
-             window.location.href = 'search.php?id_mod=' + id_producto;
-            
-        }
+    function modificar(id_producto) {
+        window.location.href = 'search.php?id_mod=' + id_producto;
+
+    }
     </script>
 
 </head>
@@ -72,10 +73,59 @@
             <div><?php echo $fecha; ?></div>
         </div>
         <div class="products-status-1">
+            <?php if (isset($_GET['id_mod'])) { ?>
+            <form id="frmAdd" name="frmAdd" action="search.php" method="post">
+                <input class="hidden" id="txtNumEmp" name="txtNumEmp" type="text" placeholder="Id"
+                    value="<?php echo @$update[0][0]; ?>" hidden>
+
+                <div class="form-group">
+                    <label for="nombre">Nombre:</label>
+                    <div class="input-group">
+                        <input class="form-control" id="nombre" name="nombre" type="text" placeholder="Nombre"
+                            value="<?php echo @$update[0][1]; ?>">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="archivo">Imagen:</label>
+                    <div class="input-group">
+                        <label for="archivo" class="custom-file-upload">
+                            Subir Imagen
+                        </label>
+                        <input type="file" id="archivo" name="archivo" accept=".jpg, .png, .pdf"
+                            value="<?php echo @$update[0][2]; ?>">
+                    </div>
+                    <p id="file-name" class="file-name"></p>
+                </div>
+
+                <div class="form-group">
+                    <label for="categoria">Categoría:</label>
+                    <div class="input-group">
+                        <select class="form-control" name="categoria" id="categoria"
+                            style="padding-bottom: 5px; padding-top: 5px;">
+                            <option value="" selected disabled>Selecciona</option>
+                            <?php echo $datos_categoria ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="cant">Cantidad:</label>
+                    <div class="input-group">
+                        <input class="form-control" id="cant" name="cant" type="number"
+                            value="<?php echo @$update[0][4]; ?>" min="1">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" id="btnUpdate" name="btnUpdate" class="btn btn-primary" value="Registrar">
+                </div>
+            </form>
+            <?php  } ?>
             <form action="search.php" class="frm-search" method="post">
                 <label class="label-search" for="busqueda">Buscar:</label>
                 <div>
-                  <input class="form-control" type="text" name="busqueda" id="busqueda">  
+                    <input class="form-control" type="text" name="busqueda" id="busqueda">
                 </div>
                 <input class="btn-search" type="submit" value="Buscar">
             </form>
@@ -88,55 +138,12 @@
                     <th>Cantidad</th>
                     <th>Acción</th>
                 </tr>
-                    <?php echo $tabla; ?>
+                <?php echo $tabla; ?>
             </table>
         </div>
         <br>
 
-        <?php if (isset($_GET['id_mod'])) { ?>
-        <form id="frmAdd" name="frmAdd" action="search.php" method="post">
-                <input class="hidden" id="txtNumEmp" name="txtNumEmp" type="text" placeholder="Id" value="<?php echo @$update[0][0]; ?>" hidden>
 
-                <div class="form-group">
-                    <label for="nombre">Nombre:</label>
-                    <div class="input-group">
-                        <input class="form-control" id="nombre" name="nombre" type="text" placeholder="Nombre" value="<?php echo @$update[0][1]; ?>">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="archivo">Imagen:</label>
-                    <div class="input-group">
-                        <label for="archivo" class="custom-file-upload">
-                            Subir Imagen
-                        </label>
-                        <input type="file" id="archivo" name="archivo" accept=".jpg, .png, .pdf" value="<?php echo @$update[0][2]; ?>">
-                    </div>
-                    <p id="file-name" class="file-name"></p>
-                </div>
-
-                <div class="form-group">
-                    <label for="categoria">Categoría:</label>
-                    <div class="input-group">
-                        <select class="form-control" name="categoria" id="categoria" style="padding-bottom: 5px; padding-top: 5px;">
-                            <option value="" selected disabled>Selecciona</option>
-                            <?php echo $datos_categoria ?>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="cant">Cantidad:</label>
-                    <div class="input-group">
-                        <input class="form-control" id="cant" name="cant" type="number" value="<?php echo @$update[0][4]; ?>" min="1">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <input type="submit" id="btnUpdate" name="btnUpdate" class="btn btn-primary" value="Registrar">
-                </div>
-            </form>
-            <?php  } ?>
     </main>
 
 </body>
