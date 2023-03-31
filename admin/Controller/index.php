387176dc39e18817b1ae9da294c304e98f045e50
IsraelMancha <mancha.israel.mijares@gmail.com>
2023-03-31 01:00:07 -0500

    act

--- admin/Controller/index.php
+++ admin/Controller/index.php
@@ -1,8 +1,8 @@
 <?php
-
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    error_reporting(0);
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
     require '../Model/fecha.php';
     require '../Model/tabla_reserva.php';
     $obj = new index();
